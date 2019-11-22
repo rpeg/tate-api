e19c090f1726b31065d9a1a1d3a423a1198a5025
Sean Johnson <sean.johnson.ca@gmail.com>
2019-11-21 16:49:27 -0800

    read filestream

--- db/seeds/development/02_art.js
+++ db/seeds/development/02_art.js
@@ -4,46 +4,12 @@ const { transformer, transfomerHeader: transformerHeader } = require('knex-csv-t
 const parse = require('csv-parse');
 const fs = require('fs');
 
-const chunks = [];
-
-
-// We can use this variable to store the final data
-let fileBuffer;
-// Read file into stream.Readable
-const fileStream = fs.createReadStream(path.join(__dirname, '../csv/the-tate-collection.csv'));
-
-// An error occurred with the stream
-fileStream.once('error', (err) => {
-  // Be sure to handle this properly!
-  console.error(err);
-}).once('end', () => {
-  // create the final data Buffer from data chunks;
-  fileBuffer = Buffer.concat(chunks);
-
-  // Of course, you can do anything else you need to here, like emit an event!
+const artWorks = [];
+const parser = parse({ delimiter: ';', columns: ['id', 'title', 'artist', 'year'] }, (err, data) => {
+  console.log(data);
 });
 
-const artWorks = [];
-
-parse(fileBuffer, {
-  trim: true,
-  skip_empty_lines: true,
-})
-// Use the readable stream api
-  .on('readable', () => {
-    let record;
-    while (record = this.read()) {
-      const art = {
-        title: record.title,
-        artist: record.artist,
-        year: record.year,
-        location: record.location,
-      };
-
-      artWorks.push(art);
-    }
-  });
+fs.createReadStream(path.join(__dirname, '/the-tate-collection.csv')).pipe(parser);
 
 
-exports.seed = (knex) => knex('art').del()
-  .then(() => knex('art').insert(artWorks));
+exports.seed = (knex) => knex('art').insert(artWorks);
